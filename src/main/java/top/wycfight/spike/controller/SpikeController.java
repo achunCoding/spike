18b82869f695b27f6c7b78315ba1ebb53fc54950
achun <wycfight@163.com>
2020-01-02 08:01:41 +0800

    增加sql文件

--- src/main/java/top/wycfight/spike/controller/SpikeController.java
+++ src/main/java/top/wycfight/spike/controller/SpikeController.java
@@ -1,7 +1,11 @@
 package top.wycfight.spike.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import top.wycfight.spike.entity.User;
+import top.wycfight.spike.service.GoodsService;
 
 /**
  * @author: wycfight@163.com
@@ -12,4 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("spike")
 public class SpikeController {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @RequestMapping("spike")
+    public String list(User user, @RequestParam(value = "goodsId")Long goodsId) {
+        // 判断是否有库存
+
+
+        // 判断用户是否已经秒杀过商品信息
+
+
+        // 进行秒杀  减库存，写订单，
+
+
+
+        return "";
+    }
 }
